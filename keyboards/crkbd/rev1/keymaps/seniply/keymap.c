1f86e7654bbf5fb68317abf6ab29ee936aaaf398
Olav Vatne <olavvatne@gmail.com>
2025-07-11 14:22:11 +0200

    feat: add homerow to extends, numbers & function layer

--- keyboards/crkbd/rev1/keymaps/seniply/keymap.c
+++ keyboards/crkbd/rev1/keymaps/seniply/keymap.c
@@ -33,6 +33,11 @@ char last_key_pressed[16] = "None";
 #define ALT_DOT ALT_T(KC_DOT)
 #define GUI_SLSH GUI_T(KC_SLSH)
 
+// Extend
+#define GUI_AE GUI_T(NO_AE)
+#define ALT_CIRC ALT_T(KC_CIRC)
+#define CTL_RABK CTL_T(KC_RABK)
+#define SFT_BSLS SFT_T(KC_BSLS)
 
 enum custom_keycodes {
     TOGGLE_OVERLAY = SAFE_RANGE,
@@ -52,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_EXT] = LAYOUT_split_3x6_3(
       _______, KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,                      KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_CAPS, _______,
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ALGR,                    KC_END,  KC_LEFT, KC_DOWN, KC_RIGHT, KC_DEL, _______,
-      _______, _______, _______, _______, _______, XXXXXXX,                    XXXXXXX, KC_BSPC, KC_TAB,  KC_APP,   KC_PSCR, _______,
+      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                    XXXXXXX, KC_BSPC, KC_TAB,  KC_APP,   KC_PSCR, _______,
                                           _______, _______, _______,    KC_ENT, _______, _______
       //                                           ^^^^^^^
   ),
@@ -66,14 +71,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM] = LAYOUT_split_3x6_3(
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_PEQL, KC_7, KC_8, KC_9,  KC_PPLS, _______,
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_PAST, KC_4, KC_5, KC_6,  KC_PMNS, _______,
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     KC_0,    KC_1, KC_2, KC_3,  KC_PSLS, _______,
+      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                     KC_0,    KC_1, KC_2, KC_3,  KC_PSLS, _______,
                                           _______, _______, _______,    _______, _______, _______
       //                                           ^^^^^^^                       ^^^^^^^
   ),
   [_FUNC] = LAYOUT_split_3x6_3(
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_F10, KC_F7, KC_F8, KC_F9,  XXXXXXX, _______,
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_F11, KC_F4, KC_F5, KC_F6,  XXXXXXX, _______,
-      _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_F12, KC_F1, KC_F2, KC_F3,  XXXXXXX, _______,
+      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                    KC_F12, KC_F1, KC_F2, KC_F3,  XXXXXXX, _______,
                                           _______, _______, _______,    _______, _______, _______
   ),
 };
